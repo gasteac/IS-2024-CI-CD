2d7141807517eaa66ca6148550bb47b70827e22e
Gaston Acosta <gasteac@gmail.com>
2024-04-30 12:35:18 -0300

    Refactor routes in App.jsx to use PublicRoute and PrivateRoute components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,25 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Home } from "./pages/Home";
+import { PublicRoute } from "./components/PublicRoute";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route
-            className="bg-black"
-            path="*"
-            element={<Navigate to="/home" replace />}
-          />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
